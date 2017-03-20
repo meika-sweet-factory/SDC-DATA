e86d4902e89c7e537f8bbb69bbb8e0410da86722
Li ZhenHui <phetsi_w@etna-alternance.net>
2017-03-20 11:59:36 +0100

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -7,12 +7,11 @@ router.get('/_search', function(req, res, next) {
     async.parallel({
 	societe: (callback) => {
 	    setTimeout(() => scrap.societe(req.query, data => callback(null, data)), 10)
+	},
+	score3: (callback) => {
+	    setTimeout(() => scrap.score3(req.query, data => callback(null, data)), 10)
 	}
     }, (error, result) => res.json(Object.assign({}, result.score3)))
 })
-
-//	score3: (callback) => {
-//	    setTimeout(() => scrap.score3(req.query, data => callback(null, data)), 10)
-//	}
 
 module.exports = router
