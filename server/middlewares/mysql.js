36b7b6b60466aba004401414ddf8a5ed835cf320
Unknown <bailli_k@etna-alternance.net>
2017-03-20 12:44:32 +0100

    clean

--- server/middlewares/mysql.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql');
-
-module.exports = mysql.createConnection({
-    host : 'database-server',
-    user : 'root',
-    password : 'root',
-    database : 'sirene'
-});
--- server/routes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express"),
-      router = express.Router();
-
-module.exports = function(app) {
-    app.get('/_search', require('./routes/api'));
-}
